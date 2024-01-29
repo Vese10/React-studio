24e673bad51df891e9e6c644193f8c367c17789c
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-01-29 10:13:15 +0100

    learning

---
+++ index.js
@@ -0,0 +1,53 @@
+function Header(){
+  return (
+    <header>
+      <nav className="nav">
+        <img src="IMG/react-logo.png" className="nav-logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function MainComponent(){
+  return (
+    <div>
+      <h1>
+        Reasons i'm exited to learn React
+      </h1>
+      <ol>
+        <li>It's usefull</li>
+        <li>It's a hireable skill</li>
+        <li>It's constantly maintained</li>
+      </ol>
+    </div>
+  )
+}
+
+function Footer(){
+ return (
+  <footer className="footer">
+    <small>©2024 Hristov development. All rights reserved.</small>
+  </footer>
+ )
+}
+
+function Page(){
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+      <Footer />
+    </div>
+    
+  )
+}
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById("root")
+)
